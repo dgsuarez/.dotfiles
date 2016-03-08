c5eb6c25fb0fd7a040c46da7f7da34197706b52c
Diego Guerra <diego.guerra.suarez@gmail.com>
2016-03-08 17:05:32 +0100

    hack to check for guake terminal

--- bashconf/termwtf.bash
+++ bashconf/termwtf.bash
@@ -1,3 +1,8 @@
-if [ "$COLORTERM" == "xfce4-terminal" ] ; then
+
+if [[ "$COLORTERM" -eq "xfce4-terminal" ]]; then
+  export TERM=xterm-256color
+elif [[ -n "$DISPLAY" ]] && [[ $(hash xprop &> /dev/null && xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2 | xargs xprop -id | grep -qi guake) ]]; then
+  # Pretty sure we are in guake...
   export TERM=xterm-256color
 fi
+
