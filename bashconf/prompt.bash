1e7a7635d91addd2bef1b3073a2c64612150320f
Diego Guerra <diego.guerra.suarez@gmail.com>
2017-10-27 10:21:40 +0200

    show update check in hg prompt

--- bashconf/prompt.bash
+++ bashconf/prompt.bash
@@ -1,6 +1,6 @@
 hg_prompt(){
-  hash hg &> /dev/null && /usr/bin/env hg prompt " (hg {branch}{:{bookmark}}) " 2> /dev/null
-}    
+  hash hg &> /dev/null && /usr/bin/env hg prompt " (hg {branch}{ {update}}) " 2> /dev/null
+}
 
 git_prompt(){
   hash __git_ps1 &>/dev/null && GIT_PS1_SHOWDIRTYSTATE='y' GIT_PS1_SHOWUNTRACKEDFILES='y'  __git_ps1 ' (git %s) '
