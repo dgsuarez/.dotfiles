3fef90ae7a664a7a41134517944e60780a3d011b
Diego Guerra <diego.guerra.suarez@gmail.com>
2024-02-16 15:24:13 +0800

    History

--- zshconf/base.sh
+++ zshconf/base.sh
@@ -1,9 +1,36 @@
-# Shared history
-HISTSIZE=10000
-SAVEHIST=10000
-HISTFILE=${HISTFILE:-$HOME/.zsh_history}
-setopt share_history
-setopt hist_ignore_dups
-setopt hist_ignore_space
-
 unsetopt beep
+
+##########
+# HISTORY
+##########
+
+HISTFILE=$HOME/.zsh_history
+HISTSIZE=50000
+SAVEHIST=50000
+
+# Immediately append to history file:
+setopt INC_APPEND_HISTORY
+
+# Record timestamp in history:
+setopt EXTENDED_HISTORY
+
+# Expire duplicate entries first when trimming history:
+setopt HIST_EXPIRE_DUPS_FIRST
+
+# Dont record an entry that was just recorded again:
+setopt HIST_IGNORE_DUPS
+
+# Delete old recorded entry if new entry is a duplicate:
+setopt HIST_IGNORE_ALL_DUPS
+
+# Do not display a line previously found:
+setopt HIST_FIND_NO_DUPS
+
+# Dont record an entry starting with a space:
+setopt HIST_IGNORE_SPACE
+
+# Dont write duplicate entries in the history file:
+setopt HIST_SAVE_NO_DUPS
+
+# Share history between all sessions:
+setopt SHARE_HISTORY
