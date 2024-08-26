f60162fd195e53d0a7baa72d586fa11511416959
Diego Guerra <diego.guerra.suarez@gmail.com>
2024-08-26 17:53:03 +0800

    Default title

--- config/gh/newpr.sh
+++ config/gh/newpr.sh
@@ -3,6 +3,9 @@ set -e
 
 BODY_FILE="$(mktemp -d)/template.md"
 PR_TEMPLATE_PATH="$PWD/.github/PULL_REQUEST_TEMPLATE.md"
+TITLE="$1"
+
+[[ -z "$TITLE" ]] && TITLE=$(git log -1 --pretty=%s)
 
 SHORT_NAME=$(git rev-parse --abbrev-ref HEAD | awk -F '-' '{ print toupper($1) "-" (($2 ~ /[0-9]/) ? $2 : "🍅") }')
 
@@ -14,4 +17,4 @@ fi
 
 "$EDITOR" "$BODY_FILE"
 
-gh pr create -a @me -d --title "[$SHORT_NAME] $1" --body-file "$BODY_FILE"
+gh pr create -a @me -d --title "[$SHORT_NAME] $TITLE" --body-file "$BODY_FILE"
