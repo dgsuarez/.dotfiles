07d215ebb4b6957752b84a7281a287e3dcd64e3b
Diego Guerra <diego.guerra.suarez@gmail.com>
2025-10-08 17:09:45 +0200

    add completions for other commands

--- zshconf/completion.sh
+++ zshconf/completion.sh
@@ -1,4 +1,11 @@
 # Completion
+
+COMPLETIONS_DIR="$HOME/.dotfiles/zshconf/completions"
+
+if [[ -d "$COMPLETIONS_DIR" ]]; then
+  fpath=("$COMPLETIONS_DIR" $fpath)
+fi
+
 autoload -U compinit; compinit
 zstyle ':completion:*' special-dirs true
 
