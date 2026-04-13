7e05f208dab5594cbc123cdd473cc1f1c500b93d
Diego Guerra <diego.guerra.suarez@gmail.com>
2026-04-13 17:53:29 +0200

    Cache compinit to avoid 24h rescans

--- zshconf/completion.sh
+++ zshconf/completion.sh
@@ -6,7 +6,13 @@ if [[ -d "$COMPLETIONS_DIR" ]]; then
   fpath=("$COMPLETIONS_DIR" $fpath)
 fi
 
-autoload -U compinit; compinit
+autoload -Uz compinit
+# Rebuild cache at most once every 24h
+if [[ -n ~/.zcompdump(#qN.mh+24) ]]; then
+  compinit
+else
+  compinit -C
+fi
 zstyle ':completion:*' special-dirs true
 
 
