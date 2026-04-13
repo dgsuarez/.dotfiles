5618dcff59d42d2caa7f5512cb19b2912e18ffed
Diego Guerra <diego.guerra.suarez@gmail.com>
2026-04-13 17:53:32 +0200

    Drop Heroku and pyenv from shell init

--- commonshconf/dev_env.sh
+++ commonshconf/dev_env.sh
@@ -1,7 +1,3 @@
-if [ -d "/usr/local/heroku" ]; then
-  export PATH="/usr/local/heroku/bin:$PATH"
-fi
-
 if [ -d "$HOME/buildkit" ]; then
   export PATH="$HOME/buildkit/bin:$PATH"
 fi
@@ -13,12 +9,6 @@ fi
 if [ -d "$HOME/.rbenv" ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
-fi
-
-if [ -d "$HOME/.pyenv" ]; then
-  export PATH="$HOME/.pyenv/bin:$PATH"
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
 fi
 
 if [ -d "$HOME/bin" ]; then
